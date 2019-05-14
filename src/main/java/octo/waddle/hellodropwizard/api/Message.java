199b6508fc667b3a5c76c190e7de7614479484dc
Krzychu_B <krzysztof.buniewicz@gmail.com>
2019-05-14 23:04:51 +0200

    Hello skeleton

---
+++ src/main/java/octo/waddle/hellodropwizard/api/Message.java
@@ -0,0 +1,26 @@
+package octo.waddle.hellodropwizard.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+public class Message{
+    private long id;
+
+    private String content;
+
+    public Message() {}
+
+    public Message(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
