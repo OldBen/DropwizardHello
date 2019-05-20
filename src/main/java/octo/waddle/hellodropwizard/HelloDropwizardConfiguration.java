3e70db824ead252c1ba89a7a87b1e1eb692e4dbd
Krzychu_B <krzysztof.buniewicz@gmail.com>
2019-05-20 23:12:59 +0200

    final CR fixes

--- src/main/java/octo/waddle/hellodropwizard/HelloDropwizardConfiguration.java
+++ src/main/java/octo/waddle/hellodropwizard/HelloDropwizardConfiguration.java
@@ -2,20 +2,11 @@ package octo.waddle.hellodropwizard;
 
 import io.dropwizard.Configuration;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
 public class HelloDropwizardConfiguration extends Configuration {
     @NotEmpty
-    @Getter
-    @Setter
     private String message;
-
-    @NotEmpty
-    @Getter
-    @Setter
-    private String name = "Stranger";
 
 }
